a05f541f21cf9da2f3bde23ed461eb878438e069
tenaciouscri <cridelve@gmail.com>
2021-10-28 13:39:43 +0200

    Added post html + views to display posts on home

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from .models import Blog
+from .models import Post
 
 
 def home(request):
     blog_posts = Blog.objects.all()
-    context = {"blog_posts": blog_posts}
+    posts = Post.objects.all()
+    context = {"blog_posts": blog_posts, "posts": posts}
     return render(request, "blog/home.html", context)
 
 
@@ -14,3 +16,8 @@ def blog_post(request, id=1):
     blog = Blog.objects.get(id=id)
     context = {"blog": blog}
     return render(request, "blog/blog_post.html", context)
+
+def post(request, id=1):
+    post = Post.objects.get(id=id)
+    context = {"post": post}
+    return render(request, "blog/post.html", context)
