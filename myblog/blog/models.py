295a765fc24a4e61d6c537442bc03e02721e906a
tenaciouscri <cridelve@gmail.com>
2021-10-28 16:11:04 +0200

    Deactivated posts for now

--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -13,17 +13,17 @@ class Blog(models.Model):
     class Meta:
         ordering = ['-date',]
 
-class Post(models.Model):
-    title = models.CharField(max_length=150)
-    body = models.TextField()
-    date = models.DateTimeField(auto_now=True)
-    content = models.TextField(default=None, blank=True)
-    author = models.CharField(max_length=150, default = "Admin")
-    def __str__(self):
-        return self.title
+# class Post(models.Model):
+#     title = models.CharField(max_length=150)
+#     body = models.TextField()
+#     date = models.DateTimeField(auto_now=True)
+#     content = models.TextField(default=None, blank=True)
+#     author = models.CharField(max_length=150, default = "Admin")
+#     def __str__(self):
+#         return self.title
     
-    class Meta:
-        ordering = ['-date',]
+#     class Meta:
+#         ordering = ['-date',]
 
 #title
 #body
