d284abebc1d37f4ec6bfc01534b3e9ee46477c4d
tenaciouscri <cridelve@gmail.com>
2021-10-29 12:32:54 +0200

    Update to pythonanywhere < GitHub pull feature

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -17,7 +17,7 @@ def update(request):
         stored on PythonAnywhere in the git.Repo() as parameter.
         Here the name of my directory is "test.pythonanywhere.com"
         '''
-        repo = git.Repo("tenaciouscri.pythonanywhere.com/") 
+        repo = git.Repo("django-blog/") 
         origin = repo.remotes.origin
 
         origin.pull()
