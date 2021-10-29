7af033cb4f8531c5d7f0a1e938a593175077c4a2
tenaciouscri <cridelve@gmail.com>
2021-10-29 11:23:09 +0200

    pythonanywhere <> git pull

---
+++ myblog/git.Repo.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python3
+
+from git import Repo
+
+# rorepo is a Repo instance pointing to the git-python repository.
+# For all you know, the first argument to Repo is a path to the repository
+# you want to work with
+repo = Repo('/Users/temporaryadmin/DHTA_Projects/django-blog/myblog')
+assert not repo.bare
