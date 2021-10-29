ad84d124cc72c4d112ae09eb4a0c53804914a595
tenaciouscri <cridelve@gmail.com>
2021-10-29 11:37:21 +0200

    static root path updated

--- myblog/myblog/settings.py
+++ myblog/myblog/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = "/home/tenaciouscri/django-blog/myblog/static"
+STATIC_ROOT = "/home/tenaciouscri/django-blog/myblog/blog/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
