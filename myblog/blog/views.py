ed67b76164d5a3d714b4de817d4e4b3b89c2c5c6
tenaciouscri <cridelve@gmail.com>
2021-10-29 13:05:00 +0200

    update to pythonanywhere < GitHub pull feature

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -7,24 +7,6 @@ from django.http import HttpResponse
 from .models import Blog
 from .models import Post
 
-# DEF FOR AUTO PULL FROM PYTHONANYWHERE
-
-@csrf_exempt
-def update(request):
-    if request.method == "POST":
-        '''
-        pass the path of the diectory where your project will be 
-        stored on PythonAnywhere in the git.Repo() as parameter.
-        Here the name of my directory is "test.pythonanywhere.com"
-        '''
-        repo = git.Repo("django-blog/") 
-        origin = repo.remotes.origin
-
-        origin.pull()
-
-        return HttpResponse("Updated code on PythonAnywhere")
-    else:
-        return HttpResponse("Couldn't update the code on PythonAnywhere")
 
 # MY OWN DEF
 
@@ -45,3 +27,17 @@ def post(request, id=1):
     post = Post.objects.get(id=id)
     context = {"post": post}
     return render(request, "blog/post.html", context)
+
+# DEF FOR AUTO PULL FROM PYTHONANYWHERE
+
+@csrf_exempt
+def update(request):
+    if request.method == "POST":
+        repo = git.Repo("django-blog/") 
+        origin = repo.remotes.origin
+
+        origin.pull()
+
+        return HttpResponse("Updated code on PythonAnywhere")
+    else:
+        return HttpResponse("Couldn't update the code on PythonAnywhere")
--- myblog/git.Repo.py
+++ myblog/git.Repo.py
@@ -5,5 +5,5 @@ from git import Repo
 # rorepo is a Repo instance pointing to the git-python repository.
 # For all you know, the first argument to Repo is a path to the repository
 # you want to work with
-repo = Repo('/home/tenaciouscri/django-blog')
+repo = Repo('/home/tenaciouscri/django-blog/myblog/')
 assert not repo.bare
