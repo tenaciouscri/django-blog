a4c700f3789234534edf4ebbd8428b676894b524
tenaciouscri <cridelve@gmail.com>
2021-12-07 15:10:47 +0100

    Added test for unapproved comment

--- myblog/blog/tests.py
+++ myblog/blog/tests.py
@@ -56,3 +56,26 @@ class SimpleTest(TestCase):
         self.assertEqual(response.status_code, 302)
         response = self.client.get(f"/blog/{blog_post.id}/")
         self.assertTrue("Some comment" in str(response.content))
+    
+    def test_unapproved_comment(self):
+        blog_post = Blog.objects.create(
+            author=self.user, title="The title", body="The body"
+        )
+        self.client.login(username="testuser", password="password")
+        response = self.client.post(
+            "/admin/blog/comment/add/",
+            {
+                "blog": blog_post.id,
+                "author": "Bad author",
+                "body": "Bad comment",
+                "created_date_0": "2021-12-07",
+                "created_date_1": "12:07:46",
+                "initial-created_date_0": "2021-12-07",
+                "initial-created_date_1": "12:07:46",
+                "approved_comment": "off",
+                "_save": "Save",
+            },
+        )
+        self.assertEqual(response.status_code, 302)
+        response = self.client.get(f"/blog/{blog_post.id}")
+        self.assertFalse("Bad comment" in str(response.content))
