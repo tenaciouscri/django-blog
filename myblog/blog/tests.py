5b7f650abdc0f10910c9253683446579a0bb4fcf
tenaciouscri <cridelve@gmail.com>
2021-12-07 13:06:06 +0100

    Black

--- myblog/blog/tests.py
+++ myblog/blog/tests.py
@@ -3,8 +3,8 @@ from django.contrib.auth.models import User
 
 from .models import Blog
 
+
 class SimpleTest(TestCase):
-    
     def setUp(self):
         self.user = User.objects.create_superuser(
             username="testuser", password="password"
@@ -16,17 +16,42 @@ class SimpleTest(TestCase):
     def test_add_blog_post(self):
         self.client.login(username="testuser", password="password")
         response = self.client.post(
-            "/admin/blog/blog/add/", {"author": self.user.id, "title": "Some title", "body": "<p>Some body</p>", "created_date_0": "2021-12-07", "created_date_1": "09:57:39", "initial-created_date_0": "2021-12-07", "initial-created_date_1": "09:57:39", "published_date_0": "2021-12-07", "published_date_1": "09:59:01", "_save": "Save"}
+            "/admin/blog/blog/add/",
+            {
+                "author": self.user.id,
+                "title": "Some title",
+                "body": "<p>Some body</p>",
+                "created_date_0": "2021-12-07",
+                "created_date_1": "09:57:39",
+                "initial-created_date_0": "2021-12-07",
+                "initial-created_date_1": "09:57:39",
+                "published_date_0": "2021-12-07",
+                "published_date_1": "09:59:01",
+                "_save": "Save",
+            },
         )
         self.assertEqual(response.status_code, 302)
         response = self.client.get("/")
         self.assertTrue("Some title" in str(response.content))
-    
+
     def test_add_comment(self):
-        blog_post = Blog.objects.create(author = self.user, title = "The title", body = "The body")
+        blog_post = Blog.objects.create(
+            author=self.user, title="The title", body="The body"
+        )
         self.client.login(username="testuser", password="password")
         response = self.client.post(
-            "/admin/blog/comment/add/", {"blog": "blog_post.id", "author": "Some author", "body": "Some comment", "created_date_0": "2021-12-07", "created_date_1": "12:07:46", "initial-created_date_0": "2021-12-07", "initial-created_date_1": "12:07:46", "approved_comment": "on", "_save": "Save"}
+            "/admin/blog/comment/add/",
+            {
+                "blog": "blog_post.id",
+                "author": "Some author",
+                "body": "Some comment",
+                "created_date_0": "2021-12-07",
+                "created_date_1": "12:07:46",
+                "initial-created_date_0": "2021-12-07",
+                "initial-created_date_1": "12:07:46",
+                "approved_comment": "on",
+                "_save": "Save",
+            },
         )
         self.assertEqual(response.status_code, 302)
         response = self.client.get("/blog/blog_post.id/")
