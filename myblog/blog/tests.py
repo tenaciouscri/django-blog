4aa097c617b414f7e74da95b3826e1c52605c9d8
tenaciouscri <cridelve@gmail.com>
2021-12-07 15:16:41 +0100

    Black

--- myblog/blog/tests.py
+++ myblog/blog/tests.py
@@ -56,7 +56,7 @@ class SimpleTest(TestCase):
         self.assertEqual(response.status_code, 302)
         response = self.client.get(f"/blog/{blog_post.id}/")
         self.assertTrue("Some comment" in str(response.content))
-    
+
     def test_unapproved_comment(self):
         blog_post = Blog.objects.create(
             author=self.user, title="The title", body="The body"
